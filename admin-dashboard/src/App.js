30fd33c803783affc38e1c64052842f9b0e2f7c1
Westhub-crypto <godwinoloja4@gmail.com>
2026-03-02 22:30:41 +0100

    Create App.js

---
+++ admin-dashboard/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+
+function App() {
+  return <Login />; // start with login page
+}
+
+export default App;
